11c2637967c44d9123e705bef452764b473c3319
sthanischhal207 <sthanischhal207@gmail.com>
2024-11-01 11:16:06 +0545

    ADDED CODES

--- Project_LPP.py
+++ Project_LPP.py
@@ -1,4 +1,3 @@
-
 from tabulate import tabulate
 from fractions import Fraction
 
@@ -8,6 +7,7 @@ array = [
     ["t(R2)", "", "", "", "", "", "", ""],
     ["Z(R3)", "", "", "", "", "", "", ""],
 ]
+c = 0
 
 
 def get_data():
@@ -40,7 +40,7 @@ def edit_data():
 
 
 def solve_simplex_table():
-    global array
+    global array, c
     column = 0
     row = 0
     while array[3][1] < 0 or array[3][2] < 0:
@@ -49,7 +49,10 @@ def solve_simplex_table():
         else:
             column = 2
         ratio(column)
+        c += 1
+        print(f"\nSimplex Table {c}\n")
         print_array()
+        print_ans(0)
         if array[1][7] < array[2][7]:
             row = 1
         else:
@@ -57,9 +60,11 @@ def solve_simplex_table():
         print_data(row, column)
         solving(row, column)
     for i in range(2):
-        array[i + 1][7] = ''    
+        array[i + 1][7] = ""
+    c += 1
+    print(f"\nSimplex Table {c}\n")
     print_array()
-    print_ans()
+    print_ans(1)
 
 
 def solving(r, col):
@@ -68,13 +73,15 @@ def solving(r, col):
         print(f"R{r}->R{r}*({Fraction(1/array[r][col]).limit_denominator(10)})")
         divide = array[r][col]
         for i in range(6):
-            array[r][i + 1] =Fraction(array[r][i + 1] / divide).limit_denominator(10) 
+            array[r][i + 1] = Fraction(array[r][i + 1] / divide).limit_denominator(10)
     for i in range(3):
         if i + 1 != r:
             divide_by = array[i + 1][col] / array[r][col]
             print(f"R{i+1}->R{i+1}-R{r}*({Fraction(divide_by).limit_denominator(10)})")
             for j in range(6):
-                array[i + 1][j + 1] = Fraction(array[i + 1][j + 1] - (array[r][j + 1] * divide_by)).limit_denominator(10)
+                array[i + 1][j + 1] = Fraction(
+                    array[i + 1][j + 1] - (array[r][j + 1] * divide_by)
+                ).limit_denominator(10)
 
 
 def print_data(r, c):
@@ -87,10 +94,12 @@ def print_data(r, c):
 def ratio(col):
     global array
     for i in range(2):
-        array[i + 1][7] = Fraction(array[i + 1][6] / array[i + 1][col]).limit_denominator(10)
+        array[i + 1][7] = Fraction(
+            array[i + 1][6] / array[i + 1][col]
+        ).limit_denominator(10)
 
 
-def print_ans():
+def print_ans(command):
     global array
     ans = ["", "", "", "", ""]
     check = [1, 2, 3]
@@ -109,9 +118,13 @@ def print_ans():
             else:
                 if ans[i] == "":
                     ans[i] = 0
-    print(
-        f"\n\nANS OF THE TABLE IS:\nx={ans[0]}\ny={ans[1]}\ns={ans[2]}\nt={ans[3]}\nZ Max={ans[4]}"
-    )
+    print(f"\nx={ans[0]}\ny={ans[1]}\ns={ans[2]}\nt={ans[3]}\nZ Max={ans[4]}")
+    if command == 0:
+        print(
+            f"\n-------IT IS NOT AN OPTIMAL SOLUTION AS IT CONTAIN NEG INTEGER IN ROW 3-------"
+        )
+    else:
+        print(f"\n--------IT IS AN OPTIMAL SOLUTION--------")
 
 
 def print_array():
@@ -121,6 +134,7 @@ def print_array():
 
 def main():
     get_data()
+    print("\n\n-----STORED DATA-----")
     print_array()
     while (
         input(
