b3d948da4e5fa2a2178e1dfc00074c5a2f1241d2
sthanischhal207 <sthanischhal207@gmail.com>
2024-11-03 14:25:25 +0545

    Update Project_LPP.py

--- Project_LPP.py
+++ Project_LPP.py
@@ -3,27 +3,80 @@ from fractions import Fraction
 
 array = [
     ["Basis", "x(C1)", "y(C2)", "s(C3)", "t(C4)", "Z(C5)", "RHS(C6)", "Ratio"],
-    ["s(R1)", "", "", "", "", "", "", ""],
-    ["t(R2)", "", "", "", "", "", "", ""],
-    ["Z(R3)", "", "", "", "", "", "", ""],
+    ["s(R1)", "", "", 1, 0, 0, "", ""],
+    ["t(R2)", "", "", 0, 1, 0, "", ""],
+    ["Z(R3)", "", "", 0, 0, 1, "", ""],
+]
+augument=[
+    ['a','b','c'],
+    ['d','e','f'],
+    ['g','h',0],
 ]
 c = 0
 
 
-def get_data():
-    global array
-    print("\n\n----------FOR GIVEN TABLE----------\n")
-    print_array()
-    for i in range(3):
-        print(f"\nEnter value for Row {i+1}:")
-        for j in range(6):
+def get_data(choice):
+    global array,augument
+    cnt=0
+    print(f"\n----------------------\n")
+    for j in [f"FOR ax + by{'<' if choice == 1 else '>'}= c",f"FOR dx + ey {'<' if choice == 1 else '>'}=  f","FOR Z= gx+hy"]:
+        print(f"\n{j}")
+        for i in range(3):
+            if cnt == 2 and i == 2: 
+                break
             while 1:
                 try:
-                    array[i + 1][j + 1] = float(input(f"R{i+1} C{j+1}: "))
+                    augument[cnt][i] = Fraction(float(input(f"{augument[cnt][i]} = "))).limit_denominator(10)
                     break
                 except ValueError:
-                    print("INVALD INPUT ENTER A NUMBER OR DECIMAL")
+                    print("INVALD INPUT ENTER A INTEGER")
                     continue
+            
+        cnt+=1
+       
+    print("\nFOR x <= 0")
+    print("FOR y <= 0")
+    
+    if choice == 2:
+        print("\n\nThe Augumented Form:")
+        print_arugument()
+        print("AFTER TRANSPOSING:")
+        transpose()
+
+    augument[2][0] = (-1)*augument[2][0]
+    augument[2][1] = (-1)*augument[2][1]   
+
+    store_data_in_table()
+    print("CONSIDERING s, t >= 0 AS SLACK VARIABLES")
+    print("\n\nSET OF EQUATIONS ARE:")
+
+    
+
+    for i in range(3):
+        k = ['s','t','Z']
+        print(f"{augument[i][0]}x+{augument[i][1]}y+{k[i]}={augument[i][2]}")
+
+def store_data_in_table():
+    global array,augument
+    for i in range(3):
+        for k in range(3):
+            j = [1,2,6] 
+            array[i+1][j[k]] = augument[i][k]
+
+import numpy as np
+def transpose():
+    global augument
+    augument = np.array(augument)  # Convert to a NumPy array
+    augument = augument.T  # Transpose the array
+    print_arugument()
+
+
+def print_arugument():
+    global augument
+    print("-"*18)
+    for row in augument:
+        print(f"|{float(row[0]):<5} {float(row[1]):<5} : {float(row[2]):<5}|")
+    print("-"*18)
 
 
 def edit_data():
@@ -98,10 +151,9 @@ def ratio(col):
             array[i + 1][6] / array[i + 1][col]
         ).limit_denominator(10)
 
-
+ans = ["", "", "", "", ""]
 def print_ans(command):
-    global array
-    ans = ["", "", "", "", ""]
+    global array,ans
     check = [1, 2, 3]
     cnt = 0
     for i in range(5):
@@ -112,12 +164,13 @@ def print_ans(command):
                         cnt += 1
                 if cnt == 2:
                     ans[i] = array[j + 1][6]
-                else:
-                    ans[i] = 0
-                cnt = 0
+                    break
+                ans[i] = 0
             else:
-                if ans[i] == "":
-                    ans[i] = 0
+                ans[i] = 0
+            cnt = 0
+
+
     print(f"\nx={ans[0]}\ny={ans[1]}\ns={ans[2]}\nt={ans[3]}\nZ Max={ans[4]}")
     if command == 0:
         print(
@@ -133,7 +186,19 @@ def print_array():
 
 
 def main():
-    get_data()
+    while 1:
+        try:
+            choice = int(input("Solving For?\n1)MAXIMUM\n2)MINIMUM\n"))
+            if choice in [1,2]:
+                get_data(choice)
+                break
+            else:
+                print("CHOOSE 1 OR 2")
+                continue
+            
+        except ValueError:
+            print("CHOOSE EITHER 1 OR 2")
+            continue
     print("\n\n-----STORED DATA-----")
     print_array()
     while (
@@ -144,6 +209,9 @@ def main():
     ):
         edit_data()
     solve_simplex_table()
+    global ans
+    if choice==2:
+        print(f"\n\n__FINAL ANSWER__\n Z {'Min' if choice == 2 else ' Max'} = {ans[-1]} at {f'({ans[0]},{ans[1]})' if choice == 1 else f'({array[3][3]},{array[3][4]})'})")
 
 
 if __name__ == "__main__":
