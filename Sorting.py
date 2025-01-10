9ec40de80d32ab6c0f6f15db3c750ea488a9d750
sthanischhal207 <sthanischhal207@gmail.com>
2025-01-10 17:34:12 +0545

    Update Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -6,7 +6,6 @@ From = input("ENTER THE DIRECTORY YOU WANT TO SORT: ")
 os.chdir(From)
 
 # File extension categories 
-#Feel Free to Add more extension as you wish
 extensions = {
     "images": [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff", ".svg", ".webp"],
     "videos": [".mp4", ".avi", ".mkv", ".mov", ".wmv", ".flv", ".webm", ".mpeg"],
@@ -14,7 +13,7 @@ extensions = {
     "word": [".doc", ".docx"],
     "excel": [".xls", ".xlsx", ".xlsm"],
     "powerpoint": [".ppt", ".pptx", ".pps", ".ppsx"],
-    "autocad": [".dwg", ".dxf"],
+    "autocad": [".dwg", ".dxf",".bak"],
     "sketchup": [".skp"],
     "photoshop": [".psd"],
     "codes": [".py", ".c"],
@@ -24,20 +23,15 @@ extensions = {
 
 # Define paths for sorted and excluded directories
 in_all = os.path.join(From, "__SORTED__")
-no_sort = os.path.join(From, "!NO SORTED!") #create A Folder name "!NO SORTED!" where you can store things you dont want it oi be sorted
 
 # Create the output directory if it doesn't exist
 os.makedirs(in_all, exist_ok=True)
 
-# Directories to exclude from sorting
-not_sort = [in_all, no_sort]
-
 # Walk through the source directory
 for dirpath, dirnames, filenames in os.walk(From):
-    # Skip directories in the not_sort list 
-    if any(dirpath.startswith(excluded) for excluded in not_sort):
+    # Skip directories in the not_sort list or their subdirectories
+    if "SORT" in dirpath.upper():
         continue
-
     for file in filenames:
         filepath = os.path.join(dirpath, file)  # Full path of the current file
 
